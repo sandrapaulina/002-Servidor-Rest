247bd4cd0f17217895bc378c50f58421a8a425e9
sandra.badillo <sandrabadmac19@gmail.com>
2021-03-11 21:09:50 -0600

    Conexion lista a BDD

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require ('body-parser');
 const app = express();
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {  //cliente, respuesta del servidor
-  res.send('<h1>Bienvenido a mi servidor REST</h1>');
+  res.send('<h1>Bienvenido a mi servidor REST (localhost)</h1>');
 });
 
 app.get('/usuario', function (req, res) {  //cliente, respuesta del servidor
@@ -59,6 +61,16 @@ app.delete('/usuario/:id', function(req, res){
       mensaje: 'Usuario eliminado con exito',
       id: id
     });
+});
+
+mongoose.connect('mongodb://localhost:27017/Cafeteria',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, (err, res) => { //SQL 3606
+if(err) throw err;
+console.log('Base de datos ONLINE');
 });
  
 app.listen(process.env.PORT, () => {
